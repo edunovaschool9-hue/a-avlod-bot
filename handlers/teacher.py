30bdfebab4cfcc45e677985c7b69fcf13b3ab756
edunovaschool9-hue <edunovaschool9@gmail.com>
2026-05-19 23:11:01 +0500

    Update teacher.py

--- handlers/teacher.py
+++ handlers/teacher.py
@@ -3,22 +3,3 @@ from aiogram.types import Message
 from config import TEACHER_ID
 
 router = Router()
-
-
-@router.message(lambda message: message.from_user.id == int(TEACHER_ID))
-async def teacher_panel(message: Message):
-
-    text = """
-👨‍🏫 O'qituvchi paneli
-
-Siz quyidagilarni boshqarishingiz mumkin:
-
-✅ Uy vazifalar
-✅ O'quvchilar
-✅ Bayt tizimi
-✅ Statistikalar
-
-Admin panel tez orada kengaytiriladi 🚀
-"""
-
-    await message.answer(text)
