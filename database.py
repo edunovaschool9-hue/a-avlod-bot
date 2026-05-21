61f4c5bc35f52396594f9afba02b3077d3ac8630
edunovaschool9-hue <edunovaschool9@gmail.com>
2026-05-21 16:50:03 +0500

    Update database.py

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ _pool = None
 async def get_pool():
     global _pool
     if _pool is None:
-        _pool = await asyncpg.create_pool(DATABASE_URL)
+        _pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=5)
     return _pool
 
 async def init_db():
